44b95e1c54163c1b9a88b476d3cf3fae4bce452c
guanhuankang.ghk <2659814334@qq.com>
2023-04-16 13:27:20 +0800

    update evaluation: 1. len 2. accuracy 3. mae 4. fbeta (beta^2 = 0.3) 5. iou 6. SA-SOR (and valid) 7. SOR (and valid)

--- evaluation/evaluation.py
+++ evaluation/evaluation.py
@@ -1,6 +1,7 @@
 import os
 import datetime
 import numpy as np
+import scipy.stats
 from PIL import Image
 from scipy import stats
 import datetime
@@ -29,6 +30,35 @@ class Evaluation:
         union = np.logical_or(pred>0, gt>0).sum()
         return inter / (union + 1e-6)
 
+    def mae(self, pred, gt):
+        DTYPE = np.uint8
+        assert pred.shape==gt.shape
+        assert pred.dtype==gt.dtype and pred.dtype==DTYPE
+        p = pred.astype(float) / 255.
+        g = gt.astype(float) / 255.
+        return np.mean(np.abs(p - g))
+
+    def acc(self, pred, gt):
+        DTYPE = np.uint8
+        assert pred.shape==gt.shape
+        assert pred.dtype==gt.dtype and pred.dtype==DTYPE
+        p = (pred > 0.0) * 1.0
+        g = (gt > 0.0) * 1.0
+        return 1.0 - np.mean(np.abs(p - g))
+
+    def fbeta(self, pred, gt):
+        DTYPE = np.uint8
+        assert pred.shape==gt.shape
+        assert pred.dtype==gt.dtype and pred.dtype==DTYPE
+        p = (pred > 0.0) * 1.0
+        g = (gt > 0.0) * 1.0
+        tp = (p * g).sum()
+        fp = (p * (1.0-g)).sum()
+        fn = ((1.0-p) * g).sum()
+        pre = tp / (tp + fp + 1e-6)
+        rec = tp / (tp + fn + 1e-6)
+        return ( 1.3 * pre * rec + 1e-6 ) / ( 0.3 * pre + rec + 1e-6 )
+
     def saSOR(self, pred, gt, thres = 0.5):
         BG = 0
         DTYPE = np.uint8
@@ -50,6 +80,7 @@ class Evaluation:
                     matrixs.append( (iou, gt_map[gval], pred_map[pval]) )
         matrixs.sort(reverse=True)
 
+        ## calc SA-SOR (and SOR)
         gt_rank = []
         pred_rank = []
         for item in matrixs:
@@ -58,37 +89,34 @@ class Evaluation:
                 gt_rank.append(g_r)
                 pred_rank.append(p_r)
 
+        sor_is_valid = True
         for r in range(1, len(gt_uni)):
             if r not in gt_rank:
                 gt_rank.append(r)
                 pred_rank.append(0)
+                sor_is_valid = False
+        sa_sor = np.corrcoef(pred_rank, gt_rank)[0, 1]
+        sor = scipy.stats.spearmanr(pred_rank, gt_rank).statistic if sor_is_valid else np.nan
 
-        return np.corrcoef(pred_rank, gt_rank)[0, 1]
-
-    def mae(self, pred, gt):
-        DTYPE = np.uint8
-        assert pred.shape==gt.shape
-        assert pred.dtype==gt.dtype and pred.dtype==DTYPE
-        p = pred.astype(float) / 255.
-        g = gt.astype(float) / 255.
-        return np.mean(np.abs(p - g))
-
-    def acc(self, pred, gt):
-        DTYPE = np.uint8
-        assert pred.shape==gt.shape
-        assert pred.dtype==gt.dtype and pred.dtype==DTYPE
-        p = (pred > 0.0) * 1.0
-        g = (gt > 0.0) * 1.0
-        return 1.0 - np.mean(np.abs(p - g))
+        return {
+            "SA-SOR": sa_sor,
+            "SOR": sor
+        }
 
     def __call__(self, name, pred_path, gt_path):
         lst = [name for name in os.listdir(gt_path) if name.endswith(".png")]
         print("#test_set={}".format(len(lst)), flush=True)
 
-        saSor_scores = []
         mae_scores = []
         acc_scores = []
-        valid = 0
+        fbeta_scores = []
+        iou_scores = []
+
+        saSor_scores = []
+        saSor_valid = 0
+        sor_scores = []
+        sor_valid = 0
+
         for name in tqdm.tqdm(lst):
             gt = np.array(Image.open(os.path.join(gt_path, name)).convert("L"))
             if os.path.exists(os.path.join(pred_path, name)):
@@ -98,12 +126,15 @@ class Evaluation:
 
             mae_scores.append(self.mae(pred, gt))
             acc_scores.append(self.acc(pred, gt))
+            fbeta_scores.append(self.fbeta(pred, gt))
+            iou_scores.append(self.IOU(pred, gt))
+
             coff = self.saSOR(pred, gt)
-            if np.isnan(coff):
-                saSor_scores.append(0)
-            else:
-                saSor_scores.append(coff)
-                valid += 1
+            sa_sor, sor = coff["SA-SOR"], coff["SOR"]
+            saSor_scores.append(0.0 if np.isnan(sa_sor) else sa_sor)
+            saSor_valid += 0 if np.isnan(sa_sor) else 1
+            sor_scores.append( 0.0 if np.isnan(sor) else sor )
+            sor_valid += 0 if np.isnan(sor) else 1
 
         ## save results
         file_name = "evaluation.json"
@@ -112,24 +143,38 @@ class Evaluation:
         history[time_indice] = {
             "name": name,
             "len": len(lst),
-            "valid": valid,
             "accuracy": np.mean(acc_scores),
             "mae": np.mean(mae_scores),
+            "fbeta": np.mean(fbeta_scores),
+            "iou": np.mean(iou_scores),
             "SA-SOR(zero)": np.mean(saSor_scores),
-            "SA-SOR(valid)": np.sum(saSor_scores) / valid
+            "sa_sor_valid": saSor_valid,
+            "SOR(zero)": np.mean(sor_scores),
+            "sor_valid": sor_valid
         }
         print(history[time_indice], flush=True)
         dumpJson(history, file_name)
 
 if __name__=="__main__":
     eval = Evaluation()
+    irsr_gt = r"D:\SaliencyRanking\dataset\irsr\Images\test\gt"
+    assr_gt = r"D:\SaliencyRanking\dataset\ASSR\ASSR\gt\test"
+
+    ## IRSR
     eval(
         name="IRSRonIRSR",
         pred_path=r"D:\SaliencyRanking\retrain_compared_results\IRSR\IRSR\prediction",
-        gt_path=r"D:\SaliencyRanking\dataset\irsr\Images\test\gt"
+        gt_path=irsr_gt
     )
+
+    ## PPA
     eval(
         name="PPAonASSR",
         pred_path=r"D:\SaliencyRanking\retrain_compared_results\PPA\ASSR\saliency_map",
-        gt_path=r"D:\SaliencyRanking\dataset\ASSR\ASSR\gt\test"
+        gt_path=assr_gt
+    )
+    eval(
+        name="PPAonIRSR",
+        pred_path=r"D:\SaliencyRanking\retrain_compared_results\PPA\IRSR\saliency_map",
+        gt_path=irsr_gt
     )
