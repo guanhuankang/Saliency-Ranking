e4e5fae5b88eb1c1c1d2f17e91dc9819fb890984
guanhuankang.ghk <2659814334@qq.com>
2023-05-02 14:36:20 +0800

    update 0502-1436

--- train.sh
+++ train.sh
@@ -1,5 +1,1 @@
-python train_net.py --config-file configs/ior_sor_base.yaml --num-gpus 1 \
- DATASETS.ROOT D:\SaliencyRanking\dataset\coco_sor \
- SOLVER.IMS_PER_BATCH 4 \
- INPUT.FT_SIZE_TRAIN 1024 \
- INPUT.FT_SIZE_TEST 1024
+python train_net.py --config-file configs/ior_sor_base.yaml --num-gpus 1 SOLVER.IMS_PER_BATCH 2
