1b33e8216cee4d91a0089bf6167ea5e8af939237
GUAN Huankang <huankguan2@v100d0.cs.cityu.edu.hk>
2023-05-09 16:25:32 +0800

    delete z_train.condor

--- z_condor.sh
+++ z_condor.sh
@@ -1,6 +1,6 @@
 executable = z_train.sh
-requirements = (CUDADeviceName == "Tesla V100-SXM2-32GB")
-request_GPUs = 4
+#requirements = (CUDADeviceName == "Tesla V100-SXM2-32GB")
+request_GPUs = 1
 error      = error.txt
 log        = output.txt
 output     = stdout
