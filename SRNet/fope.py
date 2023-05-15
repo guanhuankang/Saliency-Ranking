8b7dd4a069d4687dee2e8866503e2ef58298ac5e
guanhuankang.ghk <2659814334@qq.com>
2023-05-15 20:26:51 +0800

    push: day 05-15 （下班保存）

--- SRNet/fope.py
+++ SRNet/fope.py
@@ -7,6 +7,7 @@ from detectron2.modeling import META_ARCH_REGISTRY, build_backbone
 
 from .neck import FrcPN
 from .decoder import Mask2FormerDecoder
+from .head import MaskDecoder
 from .loss import hungarianMatcher, batch_mask_loss
 from .utils import calc_iou, debugDump
 
@@ -18,7 +19,8 @@ class FovealPeripheralNet(nn.Module):
         self.cfg = cfg
         self.backbone = build_backbone(cfg)
         self.neck = FrcPN(cfg)
-        self.transformer_decoder = Mask2FormerDecoder(cfg)
+        self.decoder = Mask2FormerDecoder(cfg)
+        self.head = MaskDecoder(cfg)
 
         self.register_buffer("pixel_mean", torch.tensor(cfg.MODEL.PIXEL_MEAN).reshape(1, -1, 1, 1), False)
         self.register_buffer("pixel_std", torch.tensor(cfg.MODEL.PIXEL_STD).reshape(1, 3, 1, 1), False)
@@ -30,41 +32,51 @@ class FovealPeripheralNet(nn.Module):
     def forward(self, batch_dict, *args, **argw):
         images = torch.stack([s["image"] for s in batch_dict], dim=0).to(self.device).contiguous()
         images = (images - self.pixel_mean) / self.pixel_std
-        feat = self.neck(self.backbone(images))  ## B, C, 2H, 2W
+
+        feats = self.backbone(images)
+        feats = self.neck(feats)
+        torch.cuda.empty_cache()
+
+        query = self.decoder(feats, deep_supervision=False)
+        feat = feats["res3"]
+        zpe = self.decoder.get_dense_pe(size=feat.shape[2::], b=len(feat)).flatten(2).transpose(-1, -2)
+        qpe = self.decoder.get_query_pos(b=len(feat))
+        pred_masks, iou_scores, obj_scores = self.head(query, feat, qpe, zpe)
         torch.cuda.empty_cache()
 
         if self.training:
             B = len(batch_dict)
-            H, W = batch_dict[0]["masks"].shape[-2::]
-            masks = [x["masks"].to(self.device) for x in batch_dict]
-            pred_masks, iou_scores, obj_scores = self.decoder(feat=feat)
-            pred_masks = F.interpolate(pred_masks, size=(H,W), mode="bilinear")
-
+            H, W = batch_dict[0]["masks"].shape[-2::]  ## target size
+            masks = [x["masks"].to(self.device) for x in batch_dict]  ## targets
+            pred_masks = F.interpolate(pred_masks, size=(H, W), mode="bilinear")  ## resize
             indices = hungarianMatcher(preds={"masks": pred_masks, "scores": obj_scores}, targets=masks)  ## list of tuples of indices
+
             stack_pred_masks = torch.cat([pred_masks[b,indices[b][0],:,:] for b in range(B)], dim=0)  ## K,H,W
             stack_tgt_masks = torch.cat([masks[b][indices[b][1]] for b in range(B)], dim=0)  ## K,H,W
             stack_iou = torch.cat([iou_scores[b,indices[b][0],0] for b in range(B)], dim=0)  ## K
-            stack_iou_gt = torch.stack([calc_iou(p.sigmoid(), m) for p, m in zip(stack_pred_masks, stack_tgt_masks)])  ## K
+            stack_iou_gt = torch.stack([calc_iou(p.sigmoid().detach(), m) for p, m in zip(stack_pred_masks, stack_tgt_masks)])  ## K
             pos = F.binary_cross_entropy_with_logits(obj_scores, torch.ones_like(obj_scores), reduction="none")
             neg = F.binary_cross_entropy_with_logits(obj_scores, torch.zeros_like(obj_scores), reduction="none")
             mat = torch.zeros_like(obj_scores)
             for b in range(B):
                 mat[b, indices[b][0], :] += 1.0
-            mask_loss = F.binary_cross_entropy_with_logits(stack_pred_masks, stack_tgt_masks)
-            obj_loss = (pos * mat).mean() + 0.1 * (neg * (1.0 - mat)).mean()
+
+            mask_loss = batch_mask_loss(stack_pred_masks, stack_tgt_masks).mean()
+            obj_loss = (pos * mat).sum() / (mat.sum() + 1e-6) + 0.1 * (neg * (1.0 - mat)).sum() / ((1.0 - mat).sum() + 1e-6)
             iou_loss = ((torch.sigmoid(stack_iou)-stack_iou_gt)**2).mean()
 
             if np.random.rand() < 0.2:
-                plst = list(stack_pred_masks[0:5].detach().float().cpu().sigmoid())
-                ioulst = stack_iou[0:5].detach().float().cpu().sigmoid().tolist()
-                gtlst = list(stack_tgt_masks[0:5].detach().float().cpu())
-                iougt = stack_iou_gt[0:5].detach().float().cpu().tolist()
+                k = 5
+                plst = list(stack_pred_masks[0:k].detach().float().cpu().sigmoid())
+                gtlst = list(stack_tgt_masks[0:k].detach().float().cpu())
+                ioulst = stack_iou[0:k].detach().float().cpu().sigmoid().tolist()
+                iougt = stack_iou_gt[0:k].detach().float().cpu().tolist()
                 debugDump(
                     output_dir=self.cfg.OUTPUT_DIR,
                     image_name="latest",
                     texts=[ioulst, iougt],
                     lsts=[plst, gtlst],
-                    size=(200, 200)
+                    size=(256, 256)
                 )
 
             return {
@@ -74,21 +86,13 @@ class FovealPeripheralNet(nn.Module):
             }
 
         else:
-            torch.cuda.empty_cache()
             ## Inference
-            pred_masks, iou_scores, obj_scores = self.decoder(feat=feat)
             results = []
             for b in range(len(batch_dict)):
                 H, W = batch_dict[b]["height"], batch_dict[b]["width"]
                 image_name = batch_dict[b].get("image_name") or "unknown_{}".format(b)
-                is_obj = torch.sigmoid(obj_scores[b, :, 0]).detach().cpu() > .5
-                masks = []
-                scores = []
-                for i, obj_ in enumerate(is_obj):
-                    if obj_:
-                        mask = F.interpolate(pred_masks[b:b+1, i:i+1], size=(H, W), mode="bilinear")[0, 0].detach().cpu()
-                        mask = mask.gt(0.0).float()
-                        masks.append(mask)
-                        scores.append(iou_scores[b, i, 0].sigmoid().detach().cpu())
-                results.append({"image_name": image_name, "masks": masks, "scores": scores, "num": len(scores)})
+                is_obj = torch.where(obj_scores[b] > .0)[0]
+                masks = F.interpolate(pred_masks[b, is_obj].float().unsqueeze(0), size=(H, W), mode="bilinear").sigmoid().cpu()  ## 1,k,H,W
+                scores = iou_scores[b, is_obj, 0].sigmoid().float().cpu()  ## k
+                results.append({"image_name": image_name, "masks": list(masks[0]), "scores": scores.tolist(), "num": len(scores)})
             return results
