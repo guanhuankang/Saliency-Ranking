db8d645b6891c12fddd14110a927b3f31c9af77a
guanhuankang.ghk <2659814334@qq.com>
2023-06-06 14:27:13 +0800

    SRFovealSA w SA: 1. similar to SRFoveal, add SA (gsa layer after each layer)

--- SRNet/__init__.py
+++ SRNet/__init__.py
@@ -1,3 +1,4 @@
 from .srnet import SRNet
 from .backbone import D2SwinTransformer
 from .srfoveal import SRFoveal
+from .srfovealsa import SRFovealSA
--- SRNet/modules/__init__.py
+++ SRNet/modules/__init__.py
@@ -3,3 +3,4 @@ from .mask_decoder import MaskDecoder
 from .gazeshift import GazeShift
 from .foveal import Foveal
 from .fovealq import FovealQ
+from .fovealqsa import FovealQSA
