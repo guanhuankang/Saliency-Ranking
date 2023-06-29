9685360c3f8b29c7951943d718015225c04696c2
guanhuankang <2659814334@qq.com>
2023-06-29 14:51:38 +0800

    register SRParallel

--- SRNet/__init__.py
+++ SRNet/__init__.py
@@ -1,5 +1,6 @@
 from .backbone import D2SwinTransformer
 from .srfovealsa import SRFovealSA
+from .srfovealparallel import SRParallel
 from .srnet import SRNet
 from .srfpn import SRFPN
 from .debug import Debug
