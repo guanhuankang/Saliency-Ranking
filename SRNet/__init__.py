dbe4e84ece0e0085c7f0b2740618778b86002fcc
guanhuankang <2659814334@qq.com>
2023-06-29 23:53:32 +0800

    add srparallel_sis

--- SRNet/__init__.py
+++ SRNet/__init__.py
@@ -1,6 +1,7 @@
 from .backbone import D2SwinTransformer
 from .srfovealsa import SRFovealSA
 from .srfovealparallel import SRParallel
+from .srparallel_sis import SRParallelSIS
 from .srnet import SRNet
 from .srfpn import SRFPN
 from .debug import Debug
