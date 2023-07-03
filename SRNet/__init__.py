eefec99e9d6be954bf77e1c5fdfc80347377383c
guanhuankang <2659814334@qq.com>
2023-07-03 14:06:19 +0800

    1. add deep supervision (q0,q4,q3) 2. aux_loss: mask_loss * 0.4 + bbox_loss * 0.4

--- SRNet/__init__.py
+++ SRNet/__init__.py
@@ -1,5 +1,6 @@
 from .backbone import D2SwinTransformer
 from .srfovealsa import SRFovealSA
+from .srfovealsa_deep import SRFovealSADeep
 from .srfovealparallel import SRParallel
 from .srparallel_sis import SRParallelSIS
 from .srparallelsa import SRParallelSA
