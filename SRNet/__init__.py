20e9c6bd0ff86a08cc2c821bb115d427221b9c6a
guanhuankang <2659814334@qq.com>
2023-07-04 22:12:13 +0800

    add deep_pe_num_masks

--- SRNet/__init__.py
+++ SRNet/__init__.py
@@ -2,6 +2,7 @@ from .backbone import D2SwinTransformer
 from .srfovealsa import SRFovealSA
 from .srfovealsa_deep import SRFovealSADeep
 from .srfovealsa_deep_pe import SRFovealSADeepPE
+from .srfovealsa_deep_pe_nm import SRFovealSADeepPENM
 from .srfovealparallel import SRParallel
 from .srparallel_sis import SRParallelSIS
 from .srparallelsa import SRParallelSA
