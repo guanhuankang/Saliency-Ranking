ae458dff8c77da01141f3ddca3969a1a18e731eb
guanhuankang <2659814334@qq.com>
2023-07-09 14:31:55 +0800

    update multiq to 2 branchs

--- SRNet/modules/multiq.py
+++ SRNet/modules/multiq.py
@@ -2,9 +2,12 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torchvision.ops.roi_align as roi_align
 from detectron2.config import configurable
 # from ..component import PositionEmbeddingSine
 from .registry import SALIENCY_INSTANCE_SEG
+from ..utils import xyhw2xyxy
+from ..component import Attention, MLPBlock, init_weights_
 
 class MultiHeadAttention(nn.Module):
     def __init__(self, embed_dim=256, num_heads=8, dropout_attn=0.0):
@@ -108,119 +111,122 @@ class Head(nn.Module):
             "attn_mask": attn_mask  ## BxHead, nq, HW
         }
 
-class SeeThroughMultiQ(nn.Module):
-    def __init__(self, embed_dim=256, mlp_layers=2):
+class ROISample(nn.Module):
+    def __init__(self, embed_dim=256, roi_windows=(4,4)):
         super().__init__()
-        self.conv_z = nn.Conv2d(embed_dim, embed_dim, 1)
-        self.fc_q = nn.Linear(embed_dim, embed_dim)
+        self.roi_windows = roi_windows
+        self.grid_embedding = nn.Parameter(torch.randn([embed_dim, roi_windows[0], roi_windows[1]]))
+    
+    def forward(self, feat, boxes):
+        """
+        Args:
+            feat: B, C, H, W
+            boxes: B, nq, 4 [xyhw in [0,1]]
+        Return:
+            roi_feat: B, nq, C, h, w
+        """
+        B, _, H, W = feat.shape
+        _, nq, _ = boxes.shape
 
+        b_index = torch.arange(B, device=boxes.device).reshape(B, 1, 1).expand(B, nq, 1)
+        boxes = torch.clamp(xyhw2xyxy(boxes), min=0.0, max=1.0)  ## B, nq, 4
+        boxes = boxes * torch.tensor([[[W, H, W, H]]], device=boxes.device)  ## B, nq, 4
+        boxes = torch.cat([b_index, boxes], dim=-1).flatten(0, 1)  ## Bxnq, 5
+
+        roi_feat = roi_align(feat, boxes, output_size=self.roi_windows)  ## Bxnq, C, *roi_windows
+        roi_feat = roi_feat + self.grid_embedding[None, :, :, :]
+        roi_feat = roi_feat.unflatten(0, (B, nq))  ## B, nq, C, h, w
+
+        return roi_feat
+
+class LookByMultiQ(nn.Module):
+    def __init__(self, embed_dim=256, num_heads=8, hidden_dim=1024, dropout_attn=0.0, dropout_ffn=0.0):
+        super().__init__()
         self.linear1 = nn.Linear(embed_dim, embed_dim)
         self.linear2 = nn.Linear(embed_dim, embed_dim)
-        self.g1 = nn.Sequential(MLP(embed_dim, embed_dim, embed_dim, mlp_layers), nn.LayerNorm
-                                (embed_dim), nn.Sigmoid())
-        self.g2 = nn.Sequential(MLP(embed_dim, embed_dim, embed_dim, mlp_layers), nn.LayerNorm
-                                (embed_dim), nn.Sigmoid())
-        self.fc1 = MLP(embed_dim, embed_dim, embed_dim, mlp_layers)
-        self.fc2 = MLP(embed_dim, embed_dim, embed_dim, mlp_layers)
-        self.fc_out = nn.Sequential(nn.Linear(embed_dim, embed_dim), nn.LayerNorm(embed_dim))
+        self.read_out = nn.Sequential(nn.Linear(embed_dim, embed_dim), nn.LayerNorm(embed_dim))
 
-        self._reset_parameters()
+        self.cross_attn1 = Attention(embedding_dim=embed_dim, num_heads=num_heads)
+        self.dropout1 = nn.Dropout(p=dropout_attn)
+        self.norm1 = nn.LayerNorm(embed_dim)
 
-    def _reset_parameters(self):
-        for p in self.parameters():
-            if p.dim() > 1:
-                nn.init.xavier_uniform_(p)
+        self.cross_attn2 = Attention(embedding_dim=embed_dim, num_heads=num_heads)
+        self.dropout2 = nn.Dropout(p=dropout_attn)
+        self.norm2 = nn.LayerNorm(embed_dim)
+        
+        self.self_attn1 = Attention(embedding_dim=embed_dim, num_heads=num_heads)
+        self.dropout3 = nn.Dropout(p=dropout_attn)
+        self.norm3 = nn.LayerNorm(embed_dim)
 
-    def forward(self, q, qpe, feat, feat_pe, bboxes, grid_embedding):
+        self.self_attn2 = Attention(embedding_dim=embed_dim, num_heads=num_heads)
+        self.dropout4 = nn.Dropout(p=dropout_attn)
+        self.norm4 = nn.LayerNorm(embed_dim)
+
+        self.ffn1 = MLPBlock(embedding_dim=embed_dim, mlp_dim=hidden_dim)
+        self.dropout5 = nn.Dropout(p=dropout_ffn)
+        self.norm5 = nn.LayerNorm(embed_dim)
+
+        self.ffn2 = MLPBlock(embedding_dim=embed_dim, mlp_dim=hidden_dim)
+        self.dropout6 = nn.Dropout(p=dropout_ffn)
+        self.norm6 = nn.LayerNorm(embed_dim)
+
+        init_weights_(self)
+
+    def forward(self, q, qpe, feat, feat_pe, roi_feat):
         """
-        Args:
-            q, qpe: B, nq, C
-            feat, feat_pe: B, C, H, W
-            bboxes: B, nq, 4 [xyhw in [0,1]]
-            grid_embedding: s1, s2, C
-        Return:
-            q_split: B, nq, s*s, C
+        q, qpe: B, nq, C
+        feat, feat_pe: B, C, H, W
+        roi_feat: B, nq, C, h, w
         """
-        s1, s2, _ = grid_embedding.shape
-        n_grids = int(s1 * s2)
+        B, nq, _, h, w = roi_feat.shape
 
-        q_split = self.fc_q(q + qpe).unsqueeze(2).expand(-1, -1, n_grids, -1)  ## B, nq, s*s, C
-        q_split = q_split + grid_embedding.flatten(0, 1).unsqueeze(0).unsqueeze(1) ## B, nq, s*s, C
-        roi_feat = self.ROI_sample(x=self.conv_z(feat+feat_pe), bboxes=bboxes, size=(s1, s2))  ## B, nq, C, s1, s2
-        roi_feat = roi_feat.flatten(3).transpose(-1, -2)  ## B, nq, s*s, C
-        k = self.linear1(q_split) * self.linear2(roi_feat)  ## B, nq, s*s, C
+        copyq = self.linear1(q).unsqueeze(2).repeat_interleave(h*w, dim=2)  ## B, nq, h*w, C
+        roi_feat = self.linear2(roi_feat.flatten(3).transpose(-1, -2))  ## B, nq, h*w, C
+        multi_q = (copyq + roi_feat).flatten(1, 2)  ## B, nq*h*w, C
+        multi_qpe = qpe.repeat_interleave(h*w, dim=1)  ## B, nq*hw, C
+        
+        z = feat.flatten(2).transpose(-1, -2)
+        zpe = feat_pe.flatten(2).transpose(-1, -2)
 
-        q = q.unsqueeze(2).expand(-1, -1, n_grids, -1) + grid_embedding.flatten(0, 1).unsqueeze(0).unsqueeze(1)  ## B, nq, s*s, C
-        v = self.ROI_sample(x=feat, bboxes=bboxes, size=(s1, s2)).flatten(3).transpose(-1, -2)  ## B, nq, s*s, C
-        out = self.g1(k) * self.fc1(q) + self.g2(k) * self.fc2(v)
-        out = self.fc_out(out)  ## B, nq, s*s, C
-        return out
+        ## multi_q branch
+        multi_q = self.norm1(multi_q + self.dropout1(self.cross_attn1(q=multi_q+multi_qpe, k=z+zpe, v=z)))
+        multi_q = multi_q.reshape(B*nq, h*w, -1)  ## B*nq, h*w, C
+        multi_q = self.norm2(multi_q + self.dropout2(self.self_attn1(q=multi_q, k=multi_q, v=multi_q)))
+        multi_q = self.norm3(multi_q + self.ffn1(multi_q))  ## B*nq, h*w, C
+        multi_q = multi_q.mean(dim=1).unflatten(0, (B, nq))  ## B, nq, C
 
-    def ROI_sample(self, x, bboxes, size):
-        """
-        Args:
-            x: B, C, H, W
-            bboxes: B, nq, 4 [xyhw in [0,1]]
-            size: (s1, s2)
-        Return:
-            roi: B, nq, C, s1, s2
-        """
-        # bboxes = 2.0 * bboxes - 1.0  ## [-1.0, 1.0]
-        s1, s2 = size
-        B, nq, _ = bboxes.shape
-
-        hs = torch.arange(s1, device=x.device, dtype=x.dtype)  ## s1
-        ws = torch.arange(s2, device=x.device, dtype=x.dtype)  ## s2
-        h_slice = (bboxes[:, :, 2] / s1).unsqueeze(2)  ## B, nq, 1
-        w_slice = (bboxes[:, :, 3] / s2).unsqueeze(2)  ## B, nq, 1
-        hs = h_slice * hs[None, None, :] + h_slice / 2.0  ## B, nq, s1
-        ws = w_slice * ws[None, None, :] + w_slice / 2.0  ## B, nq, s2
-        ys = hs.unsqueeze(3).expand(-1, -1, s1, s2)  ## B, nq, s1, s2
-        xs = ws.unsqueeze(2).expand(-1, -1, s1, s2)  ## B, nq, s1, s2
-
-        xs = xs + bboxes[:, :, 0, None, None] - bboxes[:, :, 3, None, None]/2.0
-        ys = ys + bboxes[:, :, 1, None, None] - bboxes[:, :, 2, None, None]/2.0
-        xs = torch.clamp(2.0 * xs - 1.0, min=-1.0, max=1.0)
-        ys = torch.clamp(2.0 * ys - 1.0, min=-1.0, max=1.0)
-        grids = torch.stack([xs, ys], dim=-1).flatten(0, 1)  ## Bxnq, s1, s2, 2
-        x = x.unsqueeze(1).repeat_interleave(nq, dim=0).flatten(0, 1)  ## Bxnq, C, H, W
-        return F.grid_sample(x, grid=grids, mode="bilinear").unflatten(0, (B, nq))  ## B, nq, C, s1, s2
+        ## global branch
+        q = self.norm4(q + self.dropout4(self.cross_attn2(q=q+qpe, k=z+zpe, v=z)))
+        q = self.norm5(q + self.dropout5(self.self_attn2(q=q+qpe, k=q+qpe, v=q)))
+        q = self.norm6(q + self.dropout6(self.ffn2(q)))  ## B, nq, C
+        
+        ## read out
+        q = self.read_out(q + multi_q)  ## B, nq, C
+        return q
 
 @SALIENCY_INSTANCE_SEG.register()
 class MultiQ(nn.Module):
     @configurable
     def __init__(self, num_queries=100, embed_dim=256, num_heads=8, hidden_dim=1024, dropout_attn=0.0, dropout_ffn=0.0, num_blocks=3, key_features=["res5","res4","res3"], mask_key="res2", grid_sizes=[(2,2),(3,3),(4,4)]):
         super().__init__()
+        assert len(key_features)==len(grid_sizes), f"{key_features} #len neq# {grid_sizes}"
+
         self.q = nn.Parameter(torch.zeros((1, num_queries, embed_dim)))
         self.qpe = nn.Parameter(torch.randn((1, num_queries, embed_dim)))
 
-        # self.pe_layer = PositionEmbeddingSine(embed_dim//2, normalize=True)
-        assert len(key_features) == len(grid_sizes), f"{key_features} length of neq {grid_sizes}"
-        self.grid_embeddings = nn.ParameterDict(dict(
-            (key, nn.Parameter(torch.randn(tuple(grid_size)+(embed_dim,))))  ## s1,s2,C
-            for key, grid_size in zip(key_features, grid_sizes)
-        ))
-        self.multiq_layers = nn.ModuleList([
-            SeeThroughMultiQ(embed_dim=embed_dim, mlp_layers=2)
+        self.roi_layers = nn.ModuleList([ROISample(embed_dim=embed_dim, roi_windows=gs) for gs in grid_sizes])
+        self.level_embed = nn.Embedding(len(key_features), embedding_dim=embed_dim)
+        self.lookByMultiQ_layers = nn.ModuleList([
+            LookByMultiQ(
+                embed_dim=embed_dim,
+                num_heads=num_heads,
+                hidden_dim=hidden_dim,
+                dropout_attn=dropout_attn,
+                dropout_ffn=dropout_ffn
+            )
             for _ in range(num_blocks)
         ])
-        self.self_attn_layers = nn.ModuleList(
-            [MultiHeadAttention(embed_dim=embed_dim, num_heads=num_heads, dropout_attn=dropout_attn) for _ in
-             range(num_blocks)])
-        self.cross_attn_layers = nn.ModuleList(
-            [MultiHeadAttention(embed_dim=embed_dim, num_heads=num_heads, dropout_attn=dropout_attn) for _ in
-             range(num_blocks)])
-        self.ffn_layers = nn.ModuleList(
-            [FFNLayer(embed_dim=embed_dim, hidden_dim=hidden_dim, dropout_ffn=dropout_ffn) for _ in
-             range(num_blocks)])
-        self.global_self_attn_layers = nn.ModuleList(
-            [MultiHeadAttention(embed_dim=embed_dim, num_heads=num_heads, dropout_attn=dropout_attn) for _ in
-             range(num_blocks)])
-        self.global_ffn_layers = nn.ModuleList(
-            [FFNLayer(embed_dim=embed_dim, hidden_dim=hidden_dim, dropout_ffn=dropout_ffn) for _ in
-             range(num_blocks)])
 
-        self.level_embed = nn.Embedding(len(key_features), embedding_dim=embed_dim)
         self.key_features = key_features
         self.mask_key = mask_key
         self.num_blocks = num_blocks
@@ -260,65 +266,23 @@ class MultiQ(nn.Module):
                 "attn_mask": BxHead, nq, HW [0,1 and detached]
         """
         mask_feature = feats[self.mask_key]
-
-        B, C, H, W = mask_feature.shape
-        nq = self.num_queries
         n_keys = len(self.key_features)
-        sizes = [feats[key].shape[2::] for key in self.key_features]
+        
+        q = self.q.expand(len(mask_feature), -1, -1)  ## B, nq, C
+        qpe = self.qpe.expand(len(mask_feature), -1, -1)  ## B, nq, C
 
-        q = self.q.expand(B, -1, -1)
-        qpe = self.qpe.expand(B, -1, -1)
-
-        predictions = [self.head(q=q, mask_feature=mask_feature, attn_size=sizes[0])]
+        out_size = mask_feature.shape[2::]
+        predictions = [self.head(q=q, mask_feature=mask_feature, attn_size=out_size)]
         for idx in range(self.num_blocks):
-            key = self.key_features[int(idx % n_keys)]
-            attn_mask = predictions[-1]["attn_mask"]
-            bboxes = predictions[-1]["bboxes"].sigmoid()
+            level_emb = self.level_embed.weight[idx % n_keys].reshape(1, -1, 1, 1)
+            feat = feats[self.key_features[idx % n_keys]] + level_emb
+            feat_pe = feats_pe[self.key_features[idx % n_keys]]
+            boxes = predictions[-1]["bboxes"].sigmoid()  ## B, nq, 4
 
-            # split: q, qpe, z, zpe, bboxes, grid_embedding
-            q_split = self.multiq_layers[idx](
-                q=q, 
-                qpe=qpe, 
-                feat=feats[key] + self.level_embed.weight[idx % n_keys][None, :, None, None], 
-                feat_pe=feats_pe[key], 
-                bboxes=bboxes, 
-                grid_embedding=self.grid_embeddings[key]
-            ).reshape(B*nq, C, -1).transpose(-1, -2)  ## B*nq, s*s, C
-
-            # Local Self-Attn: query, memory, memory_mask, pos = None, query_pos = None
-            q_split = self.self_attn_layers[idx](
-                query=q_split,
-                memory=q_split,
-                memory_mask=None,
-                pos=None,
-                query_pos=None
-            )  ## B*nq, s*s, C
-
-            # aggregate: query, memory, memory_mask, pos = None, query_pos = None
-            q = self.cross_attn_layers[idx](
-                query=q.flatten(0, 1).unsqueeze(1),  ## Bxnq, 1, C
-                memory=q_split,
-                memory_mask=None,
-                pos=None,
-                query_pos=None
-            ).reshape(B, nq, C)  ## B, nq, C
-
-            ## local FFN
-            q = self.ffn_layers[idx](q)
+            roi_feat = self.roi_layers[idx % n_keys](feat=feat, boxes=boxes)  ## B, nq, C, h, w
+            q = self.lookByMultiQ_layers[idx](q, qpe, feat, feat_pe, roi_feat)
             
-            # Gloabl Self-Attn: query, memory, memory_mask, pos = None, query_pos = None
-            q = self.global_self_attn_layers[idx](
-                query=q,
-                memory=q,
-                memory_mask=None,
-                pos=qpe,
-                query_pos=qpe
-            )
-
-            # Global FFN
-            q = self.global_ffn_layers[idx](q)
-
-            predictions.append(self.head(q=q, mask_feature=mask_feature, attn_size=sizes[(idx+1) % n_keys]))
+            predictions.append(self.head(q=q, mask_feature=mask_feature, attn_size=out_size))
 
         out = predictions[-1]
         aux = predictions[0:-1]
--- train.sh
+++ train.sh
@@ -1,2 +1,2 @@
 nvidia-smi
-python train_net.py --config-file configs/multiq_swinb.yaml MODEL.SIS_HEAD.NAME "MultiQ" MODEL.GAZE_SHIFT_HEAD.NAME "GazeShift" SOLVER.IMS_PER_GPU 1 SOLVER.STEPS "(50,80)" SOLVER.MAX_ITER 100 OUTPUT_DIR output/debug SOLVER.BASE_LR 0.00001 DATALOADER.NUM_WORKERS 1
+python train_net.py --config-file configs/multiq_swinb.yaml SOLVER.IMS_PER_GPU 1 SOLVER.STEPS "(500,800)" SOLVER.MAX_ITER 1000 OUTPUT_DIR output/debug SOLVER.BASE_LR 0.00001 DATALOADER.NUM_WORKERS 1
